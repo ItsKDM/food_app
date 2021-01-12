286bb9458168bc47a2193e5b42ae8ebba7bc945f
ItsKDM <darshangupta.0602@gmail.com>
2021-01-12 10:38:08 +0530

    first commit

---
+++ lib/screens/home/components/app_bar.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:food_app/constants.dart';
+
+AppBar homeAppBar(BuildContext context) {
+  return AppBar(
+    backgroundColor: Colors.white,
+    elevation: 0,
+    leading: IconButton(
+      icon: SvgPicture.asset("assets/icons/menu.svg"),
+      onPressed: () {},
+    ),
+    title: RichText(
+      text: TextSpan(
+        // ignore: deprecated_member_use
+        style: Theme.of(context)
+            .textTheme
+            .headline6
+            // .title
+            .copyWith(fontWeight: FontWeight.bold),
+        children: [
+          TextSpan(text: "Eat", style: TextStyle(color: ksecondaryColor)),
+          TextSpan(
+            text: "Food",
+            style: TextStyle(color: kPrimaryColor),
+          ),
+        ],
+      ),
+    ),
+    actions: <Widget>[
+      IconButton(
+        icon: SvgPicture.asset("assets/icons/notification.svg"),
+        onPressed: () {},
+      ),
+    ],
+  );
+}
